dda673609e2b40c4da90202a9194c839175dd393
ikartikeysharma <ikartikeysharma@gmail.com>
2025-07-25 04:36:24 +0000

    ok

--- gitnote/.idx/dev.nix
+++ gitnote/.idx/dev.nix
@@ -1,26 +1,23 @@
-{pkgs}: {
-  channel = "stable-24.05";
+{ pkgs, ... }:
+{
   packages = [
     pkgs.nodejs_20
   ];
-  idx.extensions = [
-    
-  ];
-  idx.previews = {
-    previews = {
-      web = {
-        command = [
-          "npm"
-          "run"
-          "dev"
-          "--"
-          "--port"
-          "$PORT"
-          "--hostname"
-          "0.0.0.0"
-        ];
-        manager = "web";
-      };
-    };
+
+  idx.previews.web = {
+    command = [
+      "npm"
+      "run"
+      "dev"
+      "--"
+      "--port"
+      "$PORT"
+      "--hostname"
+      "0.0.0.0"
+    ];
+    manager = "web";
   };
+
+  # You can add more configurations here, such as:
+  # idx.extensions = [ pkgs.vscode-extensions.<extension-name> ];
 }
